9a3cd6433f1a81c9c28cbb9263de46e30fa891ba
Michael Campbell <buildermancampbell@aol.com>
2019-05-23 14:12:48 -0400

    Add files via upload

---
+++ compile.js
@@ -0,0 +1,164 @@
+//Programmed by https://github.io/RainbowAsteroids
+function saveAs(uri, filename) {
+  var link = document.createElement('a');
+  if (typeof link.download === 'string') {
+    link.href = uri;
+    link.download = filename;
+
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+
+    //simulate click
+    link.click();
+
+    //remove the link when done
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
+}
+
+class Answer { //Awnser data type. Stores the answer option and the result
+    constructor(parent){ //Parent should be an ordered list
+        this.parent = parent;
+
+        this.mainElement = document.createElement('li');
+        this.parent.appendChild(this.mainElement);
+		
+		//create user inputs
+        this.option = document.createElement('input'); this.option.type = "text";
+        this.result = document.createElement('input'); this.option.type = "text";
+        this.mainElement.appendChild(this.option);
+        this.mainElement.insertAdjacentHTML("beforeend"," Output: ");
+        this.mainElement.appendChild(this.result);
+        this.mainElement.insertAdjacentHTML("beforeend","<br>");
+    }
+
+    delete() {this.parent.removeChild(this.mainElement);} //deletes itself
+}
+
+class Question {
+    constructor(parent,id){ //Parent should be Quiz.mainElement
+        this.parent = parent;
+
+        this.mainElement = document.createElement('div'); //We're going to put EVERYTHING in here
+        this.parent.appendChild(this.mainElement); //Make sure we don't lose mommy at the store
+
+        //Question title
+        this.mainElement.insertAdjacentHTML("beforeend","Question Title: ");
+        this.questionTitle = document.createElement('input');
+        this.questionTitle.type = "text";
+        this.mainElement.appendChild(this.questionTitle)
+        this.mainElement.insertAdjacentHTML("beforeend","<br>");
+
+        //We're storing answers here
+        this.answersOL = document.createElement("ol");
+        this.mainElement.appendChild(this.answersOL);
+        this.answersOL.type = "A"; //This type makes the OL read as A, B, C, D
+        this.answers = new Array(); // This is the good stuff of the good stuff
+        for (var i = 0; i < 4; i++) {this.addAnswerOption();} //How nice for you to make answers for us!
+
+        //add and remove buttons
+        var addButton = document.createElement('input'); addButton.type = "button"
+        var removeButton = document.createElement('input'); removeButton.type = "button"
+        addButton.value = "Add Answer Option";
+        removeButton.value = "Remove Answer Option";
+        var buttonLi = document.createElement('li');
+        buttonLi.insertAdjacentHTML("beforeend","<br>")
+        buttonLi.appendChild(addButton);
+        buttonLi.insertAdjacentHTML('beforeend',' ');
+        buttonLi.appendChild(removeButton);
+
+        var buttonUL = document.createElement('ul');
+        this.mainElement.appendChild(buttonUL);
+
+        buttonLi.style = "list-style-type: none;";
+        buttonUL.appendChild(buttonLi);
+
+        addButton.onclick = function() {quiz.questions[id].addAnswerOption();}
+        removeButton.onclick = function() {quiz.questions[id].removeAnswerOption();}
+    }
+
+    addAnswerOption() {
+        this.answers.push(
+            new Answer(this.answersOL)
+        );
+    } //adds an answer option
+    removeAnswerOption() { //gets rid of an answer option
+        this.answers[this.answers.length-1].delete();
+        this.answers.pop();
+    }
+    delete() {this.parent.removeChild(this.mainElement);} //deletes itself
+}
+
+class Quiz { //This is the quiz
+    constructor(){
+        this.mainElement = document.createElement('div'); //We're going to put EVERYTHING in here
+        document.body.appendChild(this.mainElement); //Lets add it to the body
+
+        //Quiz title and author
+        this.mainElement.insertAdjacentHTML("beforeend","Quiz Title: ");
+        this.quizTitle = document.createElement('input'); this.quizTitle.type = "text";
+        this.mainElement.appendChild(this.quizTitle);
+        this.mainElement.insertAdjacentHTML("beforeend"," Quiz Author: ");
+        this.author = document.createElement('input'); this.author.type = "text";
+        this.mainElement.appendChild(this.author);
+        this.mainElement.insertAdjacentHTML("beforeend","<br><br>") //Add line breaks for questions
+
+        this.questions = new Array(); // This is the good stuff, the questions
+        this.questions.push(new Question(this.mainElement, this.questions.length)); // Automagicly add the first question
+
+        //add and remove buttons
+        var addButton = document.createElement('input'); addButton.type = "button"
+        var removeButton = document.createElement('input'); removeButton.type = "button"
+        var finishButton = document.createElement('input'); finishButton.type = "button"
+        addButton.value = "Add Question";
+        removeButton.value = "Remove Question";
+        finishButton.value = "Finish";
+        this.buttonElement = document.createElement('span');
+
+        //Adds buttons
+		this.buttonElement.appendChild(addButton);
+        this.buttonElement.insertAdjacentHTML('beforeend',' ');
+        this.buttonElement.appendChild(removeButton);
+        this.buttonElement.insertAdjacentHTML('beforeend','<br><br>');
+        this.buttonElement.appendChild(finishButton);
+
+        this.mainElement.appendChild(this.buttonElement);
+
+        addButton.onclick = function() {quiz.addQuestion();}
+        removeButton.onclick = function() {quiz.removeQuestion();}
+        finishButton.onclick  = function() {quiz.compile()}
+    }
+
+    addQuestion() { //Create a new question
+        this.mainElement.removeChild(this.buttonElement);
+        this.questions.push(new Question(this.mainElement, this.questions.length));
+        this.mainElement.appendChild(this.buttonElement);
+    }
+    removeQuestion() { //Get rid of a new question
+        this.questions[this.questions.length-1].delete();
+        this.questions.pop();
+    }
+
+    compile() {
+        //This is the JSON we are going to give to the user
+        var save_quiz = {"title":this.quizTitle.value, "author":this.author.value, "questions":new Array()};
+        this.questions.forEach(function(question) {
+            let answers = {}
+            question.answers.forEach(function(answer) {
+                answers[answer.option.value] = answer.result.value
+            })
+            save_quiz["questions"].push({
+                "title":question.questionTitle.value,
+                "answers":answers
+            })
+        })
+        saveAs("data:application/json;charset=utf-8;base64,"+window.btoa(JSON.stringify(save_quiz,"",4)), quiz.quizTitle.value+".json");
+    }
+}
+
+var quiz;
+function init(){
+    quiz = new Quiz();
+}
---
+++ quiz.js
@@ -0,0 +1,72 @@
+//Programmed by https://github.io/RainbowAsteroids
+var file;
+var mainElement = document.createElement('div');
+function readFile(element) { //Handle the inputted file
+    file = element.files[0];
+    var reader = new FileReader();
+    var quiz = ""
+    reader.onload = function() {init(reader.result);}
+    reader.readAsText(file); //Send it on to the quiz creator
+}
+
+function init(data) {
+
+    document.body.removeChild(document.getElementById('upload'));
+    document.body.appendChild(mainElement);
+    quizData = JSON.parse(data); //Parse the data for JSON
+    console.log("%cInputted JSON data:", "background-color: yellow; font-size: large");
+    console.log(quizData); //Debug Output
+
+    mainElement.insertAdjacentHTML("beforeend","<h1>"+quizData["title"]+"</h1>"); //Quiz Metadata
+    mainElement.insertAdjacentHTML("beforeend","<h2>"+quizData["author"]+"</h2>");
+
+    let i = -1
+    quizData["questions"].forEach(function(question) { //Setup the question
+        i++
+        mainElement.insertAdjacentHTML("beforeend","<h3>"+question["title"]+"</h3>");
+        let options = document.createElement("ol"); options.type = "A"
+        for (option in question["answers"]) {
+            let li = document.createElement("li");
+            let input = document.createElement("input");
+            input.type = "radio"; input.name = question["title"]; input.value = option; input.id = i.toString();
+            li.appendChild(input); li.insertAdjacentHTML("beforeend",option);
+            options.appendChild(li);
+        } mainElement.appendChild(options); //Append this to the body
+    });
+    var finish = document.createElement("input");
+    finish.type = "button";
+    finish.value = "Finish";
+    finish.onclick = function() {results(quizData)};
+    mainElement.appendChild(finish); mainElement.insertAdjacentHTML("beforeend", "<br><br>")
+}
+
+function results(data) {
+    for (element of mainElement.getElementsByClassName("error")) {
+        mainElement.removeChild(element);
+    }
+    var inputs = new Array();
+    var pass = true;
+    data["questions"].forEach(function(question) {
+        let answered = false;
+        document.getElementsByName(question["title"]).forEach(function(input) {
+            if (input.checked) {inputs.push(input.cloneNode());answered = true;return;}
+        });
+        pass = answered; //If user didn't answer any questions, don't execute the second half of this function
+        if (!answered) { //make sure user answered the questions
+            mainElement.insertAdjacentHTML("beforeend",'<b class="error">You did not answer question "'+question["title"]+'"!<br></b>');
+        }
+    }); if (!pass) {return;} //Stop this function if there are unanswered questions
+
+    document.body.removeChild(mainElement);
+    mainElement = document.createElement("div");
+    document.body.appendChild(mainElement);
+    document.body.insertAdjacentHTML("afterbegin", "<h2>Quiz Results</h2>");
+    var output = document.createElement('p'); mainElement.appendChild(output);
+
+    inputs.forEach(function(element) {
+        console.log(element.id);
+        output.insertAdjacentHTML("beforeend", data["questions"][Number(element.id)]["answers"][element.value]+" ");
+    })
+    output.insertAdjacentHTML("beforeend","<br>");
+    mainElement.insertAdjacentHTML("beforeend","<hr><footer>Thank you for taking the survey \""+data["title"]+"\" by "+data["author"]+" and for using <a href="https://github.com/RainbowAsteroids/ProgressiveSurvey"><b>Progressive Survey</b></a>!</footer>")
+}
